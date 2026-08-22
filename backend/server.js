37c8a183a25733b3218351bc77270ca75f01a2da
K REETHIKA REDDY <reethikareddy@Ks-MacBook-Air.local>
2026-08-22 16:39:22 +0530

    Complete authentication and dashboard

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
